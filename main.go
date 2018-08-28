12e1e0a2f4fb9e99bb229c68fefc4ba81e09d16d
shishijie <shishijie@live.com>
2018-08-28 17:07:27 +0800

    1.0 增加 handle http调用

--- main.go
+++ main.go
@@ -187,10 +187,37 @@ func main() {
 	go startRpcServer()
 	tryRpcClient()
 
+	//handler := MyHandler{"shoes": 50, "socks": 5}
+	//mux := http.NewServeMux()
+	//mux.Handle("/list", http.HandlerFunc(handler.list))
+	//mux.Handle("/price", http.HandlerFunc(handler.price))
+	//log.Fatal(http.ListenAndServe("localhost:9000", mux))
+
 
 	http.HandleFunc("/", IndexHandler)
 	http.ListenAndServe("127.0.0.1:8000", nil)
 
+
 }
 
+type dollars float32
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
 
+type MyHandler map[string]dollars
+func (self MyHandler) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range self {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+func (self MyHandler) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := self[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
